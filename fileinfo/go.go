e81fe3ed3370dd5b371e1a590f271c7b051cffbd
bayashi <bayashi@cpan.org>
2023-05-02 21:49:58 +0000

    refactor more

--- fileinfo/go.go
+++ fileinfo/go.go
@@ -12,16 +12,16 @@ type info struct {
 	m    map[string]struct{}
 }
 
-type p struct {
-	public  *info
-	private *info
+type organizer struct {
+	structs   [2]*info
+	constants [2]*info
+	functions [2]*info
 }
 
-type organizer struct {
-	structs   *p
-	constants *p
-	functions *p
-}
+const (
+	PRIV = 0
+	PUB  = 1
+)
 
 func defInfo() *info {
 	return &info{list: []string{}, m: map[string]struct{}{}}
@@ -37,9 +37,9 @@ func GoInfo(args *Args) (*FileInfo, error) {
 	//ast.Print(fset, fileAst)
 
 	var i = organizer{
-		structs:   &p{public: defInfo(), private: defInfo()},
-		constants: &p{public: defInfo(), private: defInfo()},
-		functions: &p{public: defInfo(), private: defInfo()},
+		structs:   [2]*info{defInfo(), defInfo()},
+		constants: [2]*info{defInfo(), defInfo()},
+		functions: [2]*info{defInfo(), defInfo()},
 	}
 
 	ast.Inspect(fileAst, func(n ast.Node) bool {
@@ -66,66 +66,59 @@ func GoInfo(args *Args) (*FileInfo, error) {
 	}, nil
 }
 
+func idx(isExported bool) int {
+	if isExported {
+		return 1 // public
+	}
+
+	return 0 // private
+}
+
 func goInfoProcessIdent(o *ast.Object, i *organizer) {
 	switch o.Kind {
 	case ast.Fun:
-		if ast.IsExported(o.Name) {
-			if _, isExists := i.functions.public.m[o.Name]; !isExists {
-				i.functions.public.list = append(i.functions.public.list, o.Name)
-				i.functions.public.m[o.Name] = struct{}{}
-			}
-		} else {
-			if _, isExists := i.functions.private.m[o.Name]; !isExists {
-				i.functions.private.list = append(i.functions.private.list, o.Name)
-				i.functions.private.m[o.Name] = struct{}{}
-			}
+		f := i.functions[idx(ast.IsExported(o.Name))]
+		if _, isExists := f.m[o.Name]; !isExists {
+			f.list = append(f.list, o.Name)
+			f.m[o.Name] = struct{}{}
 		}
 	case ast.Typ:
-		if ast.IsExported(o.Name) {
-			if _, isExists := i.structs.public.m[o.Name]; !isExists {
-				i.structs.public.list = append(i.structs.public.list, o.Name)
-				i.structs.public.m[o.Name] = struct{}{}
-			}
-		} else {
-			if _, isExists := i.structs.private.m[o.Name]; !isExists {
-				i.structs.private.list = append(i.structs.private.list, o.Name)
-				i.structs.private.m[o.Name] = struct{}{}
-			}
+		s := i.structs[idx(ast.IsExported(o.Name))]
+		if _, isExists := s.m[o.Name]; !isExists {
+			s.list = append(s.list, o.Name)
+			s.m[o.Name] = struct{}{}
 		}
 	case ast.Con:
-		if ast.IsExported(o.Name) {
-			if _, isExists := i.constants.public.m[o.Name]; !isExists {
-				i.constants.public.list = append(i.constants.public.list, o.Name)
-				i.constants.public.m[o.Name] = struct{}{}
-			}
-		} else {
-			if _, isExists := i.constants.private.m[o.Name]; !isExists {
-				i.constants.private.list = append(i.constants.private.list, o.Name)
-				i.constants.private.m[o.Name] = struct{}{}
-			}
+		c := i.constants[idx(ast.IsExported(o.Name))]
+		if _, isExists := c.m[o.Name]; !isExists {
+			c.list = append(c.list, o.Name)
+			c.m[o.Name] = struct{}{}
 		}
 	}
 }
 
 func buildDescriptions(i *organizer, args *Args) *[]string {
 	descriptions := []string{}
-	if len(i.structs.public.list) > 0 {
-		descriptions = append(descriptions, "Struct: "+strings.Join(i.structs.public.list, ", "))
+
+	if len(i.structs[PUB].list) > 0 {
+		descriptions = append(descriptions, "Struct: "+strings.Join(i.structs[PUB].list, ", "))
 	}
-	if args.ShowAll && len(i.structs.private.list) > 0 {
-		descriptions = append(descriptions, "struct: "+strings.Join(i.structs.private.list, ", "))
+	if args.ShowAll && len(i.structs[PRIV].list) > 0 {
+		descriptions = append(descriptions, "struct: "+strings.Join(i.structs[PRIV].list, ", "))
 	}
-	if len(i.functions.public.list) > 0 {
-		descriptions = append(descriptions, "Func: "+strings.Join(i.functions.public.list, ", "))
+
+	if len(i.functions[PUB].list) > 0 {
+		descriptions = append(descriptions, "Func: "+strings.Join(i.functions[PUB].list, ", "))
 	}
-	if args.ShowAll && len(i.functions.private.list) > 0 {
-		descriptions = append(descriptions, "func: "+strings.Join(i.functions.private.list, ", "))
+	if args.ShowAll && len(i.functions[PRIV].list) > 0 {
+		descriptions = append(descriptions, "func: "+strings.Join(i.functions[PRIV].list, ", "))
 	}
-	if len(i.constants.public.list) > 0 {
-		descriptions = append(descriptions, "Const: "+strings.Join(i.constants.public.list, ", "))
+
+	if len(i.constants[PUB].list) > 0 {
+		descriptions = append(descriptions, "Const: "+strings.Join(i.constants[PUB].list, ", "))
 	}
-	if args.ShowAll && len(i.constants.private.list) > 0 {
-		descriptions = append(descriptions, "const: "+strings.Join(i.constants.private.list, ", "))
+	if args.ShowAll && len(i.constants[PRIV].list) > 0 {
+		descriptions = append(descriptions, "const: "+strings.Join(i.constants[PRIV].list, ", "))
 	}
 
 	return &descriptions
